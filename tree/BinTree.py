e0c29737d8e7955d01fc64f6fb95f558fd9e332e
akitaFriday <377607337@qq.com>
2019-04-24 17:22:04 +0800

    哈夫曼树

--- tree/BinTree.py
+++ tree/BinTree.py
@@ -1,8 +1,72 @@
+class QueueUnderFlowError(ValueError):
+    def __init__(self, reason):
+        self.reason = reason
+    
+    def getMessage(self):
+        print("*********" + self.reason + "**********")
+
+
+# 队列的类 : 非循环队列
+class Queue():
+    def __init__(self, init_len = 8):
+        self._len = init_len
+        # 初始化一个有确定长度的列表
+        self._elems = [0] * init_len
+        # 表头元素下标
+        self._head = 0
+        # 表中元素个数
+        self._num = 0
+    
+    def is_empty(self):
+        return self._num == 0
+
+    def peek(self):
+        try:
+            if self._num != 0:
+                return self._elems[self._head]
+            else:
+                raise QueueUnderFlowError("队空")
+        except QueueUnderFlowError as qe:
+            qe.getMessage()
+    
+    def dequeue(self):
+        try:
+            if self._num == 0:
+                raise QueueUnderFlowError("队空")
+            e = self._elems[self._head]
+            # 出队操作, 头元素下标后移
+            self._head = (self._head + 1) % self._len
+            self._num -= 1
+            return e
+        except QueueUnderFlowError as qe:
+            qe.getMessage()
+    
+    def enqueue(self, e):
+        try:
+            if self._len == self._num:
+                self._extend()
+            # 队尾的空位位置, 是头元素下标与元素总数之和 对列表长度取余的结果
+            self._elems[(self._head + self._num) % self._len] = e
+            self._num += 1
+        except QueueUnderFlowError as qe:
+            qe.getMessage()
+    
+    def _extend(self):
+        
+        old_len = self._len
+        # 每次增长原有容量的两倍
+        self._len *= 2
+        new_elems = [0] * self._len
+        # 把旧队列中的所有元素转移到新队列中
+        for i in range(old_len):
+            new_elems[i] = self._elems[(self._head + i) % old_len]
+        self._elems, self._head = new_elems, 0
+
+
 """
     二叉树的实现
     Author: weiyc
 """
-from stackAndQueue.queueByLineTable import Queue
 
 # 二叉树节点类 , 整个结构是基于递归定义的
 # 也就是说, left 或者 right 表示的可以是一个BinTreeNode对象
@@ -41,13 +105,45 @@ def preRootOrder(tree, operation):
     preRootOrder(tree.right, operation)
 
 # 宽度优先遍历
+def levelOrder(tree, operation):
+    operation(tree.data)
+    levelOrderQueue = Queue()
+    levelOrderQueue.enqueue(tree)
+    while not levelOrderQueue.is_empty():
+        node = levelOrderQueue.dequeue()
+        # 如果弹出的树节点为空, 直接跳过
+        if node is None:
+            continue
+        levelOrderQueue.enqueue(tree.left)
+        levelOrderQueue.enqueue(tree.right)
 
+# 非递归的先根序遍历 ,利用栈实现
+def preRootOrderByStack(tree, operation):
+    s = Stack()
+    while tree is not None or not s.is_empty():
+        while tree is not None:
+            operation(tree.data)
+            # 右子树入栈
+            s.push(tree.right)
+            # 从左子树不断深入
+            tree = tree.left
+        # 遇到叶子节点后回溯，从最近的右子树开始
+        tree = s.pop()
+
+# 递归的后序遍历，利用栈实现
+def backRootOrderByStack(tree, operation):
+    s = Stack()
+    while tree is not None or not s.is_empty():
+        while tree is not None:
+            s.push(tree)
+            tree = tree.left if  tree.left is not None else tree.right
+        tree = s.pop()
+        operation(tree.data)
+        if not s.is_empty() and s.top().left == tree:
+            t = s.top().right
+        else:
+            t = None
 
 def printData(elem):
     print(elem, end="---->")
 
-tree = BinTreeNode(1, BinTreeNode(2), BinTreeNode(3))
-preRootOrder(tree, printData)
-
-queue = Queue()
-queue.is_empty()
---
+++ tree/huffManTree.py
@@ -0,0 +1,54 @@
+"""
+    哈夫曼树
+    哈夫曼树构造的思路:
+        1. 先把指定集合的所有元素建立成单点二叉树，二叉树根节点的权值为元素值
+        2. 把所有的单点二叉树放到优先队列中，排序规则按照根节点权值大小
+        3. 从优先队列中出队两个权值最小的二叉树，构建一个新的二叉树，新二叉树根节点的权值为左右子树根节点权值的和
+        4. 新二叉树入优先队列
+        5. 重复 3，4步骤直到优先队列中只剩下一个元素
+        6. 出队元素
+"""
+from BinTree import *
+from priorityQueue import *
+
+# 对二叉树节点类的扩充
+class HuffmanTreeNode(BinTreeNode):
+    # 节点比较大小，实际比较的是节点的data属性值的大小
+    def __lt__(self, otherNode):
+        return self.data < otherNode.data
+    
+    def __le__(self, otherNode):
+        return self.data < otherNode.data
+
+
+# 对优先队列的扩充
+class HuffmanPriorityQueue(PriorityQueue):
+    def number(self):
+        return len(self._elems)
+
+
+# 构造哈夫曼树的方法 参数为 实数集合
+def createHuffmanTree(objSets):
+    trees = HuffmanPriorityQueue()
+    for i in objSets:
+        trees.enQueue(HuffmanTreeNode(i))
+    # 在队列元素不少于1时反复执行
+    while trees.number() > 1:
+        # 出队两个元素，因为优先队列的定义是从小到大排列，所以出队的两个一定是最小的两个
+        child1 = trees.deQueue()
+        child2 = trees.deQueue()
+        # 两个元素的权值相加
+        value = child1.data + child2.data
+        # 新权值与左右子树构建新的二叉树，然后入队
+        trees.enQueue(HuffmanTreeNode(value, child1, child2))
+    return trees.deQueue()
+
+
+numSets = (2, 3, 7, 10, 4, 2, 5)
+
+tree = createHuffmanTree(numSets)
+
+def operation(elem):
+    print(elem , end=" ")
+    print("--->", end=" ")
+preRootOrder(tree, operation)
